74abcf484a3b9e27148c6df30a55784f936b02df
Felipe Zerbini <felipezerbini13@gmail.com>
2025-12-23 19:42:52 -0300

    feat: adiciona script de extração de dados climáticos

---
+++ src/extracao.py
@@ -0,0 +1,49 @@
+import openmeteo_requests
+
+import pandas as pd
+import requests_cache
+from retry_requests import retry
+
+# Setup the Open-Meteo API client with cache and retry on error
+cache_session = requests_cache.CachedSession('.cache', expire_after = -1)
+retry_session = retry(cache_session, retries = 5, backoff_factor = 0.2)
+openmeteo = openmeteo_requests.Client(session = retry_session)
+
+# Make sure all required weather variables are listed here
+# The order of variables in hourly or daily is important to assign them correctly below
+url = "https://archive-api.open-meteo.com/v1/archive"
+params = {
+	"latitude": -20.3194,
+	"longitude": -40.3378,
+	"start_date": "2020-01-01",
+	"end_date": "2025-12-01",
+	"daily": ["temperature_2m_mean", "precipitation_sum"],
+	"timezone": "America/Sao_Paulo",
+}
+responses = openmeteo.weather_api(url, params=params)
+
+# Process first location. Add a for-loop for multiple locations or weather models
+response = responses[0]
+print(f"Coordinates: {response.Latitude()}°N {response.Longitude()}°E")
+print(f"Elevation: {response.Elevation()} m asl")
+print(f"Timezone: {response.Timezone()}{response.TimezoneAbbreviation()}")
+print(f"Timezone difference to GMT+0: {response.UtcOffsetSeconds()}s")
+
+# Process daily data. The order of variables needs to be the same as requested.
+daily = response.Daily()
+daily_temperature_2m_mean = daily.Variables(0).ValuesAsNumpy()
+daily_precipitation_sum = daily.Variables(1).ValuesAsNumpy()
+
+daily_data = {"date": pd.date_range(
+	start = pd.to_datetime(daily.Time(), unit = "s", utc = True),
+	end =  pd.to_datetime(daily.TimeEnd(), unit = "s", utc = True),
+	freq = pd.Timedelta(seconds = daily.Interval()),
+	inclusive = "left"
+)}
+
+daily_data["temperature_2m_mean"] = daily_temperature_2m_mean
+daily_data["precipitation_sum"] = daily_precipitation_sum
+
+daily_dataframe = pd.DataFrame(data = daily_data)
+daily_dataframe.to_parquet("daily_data.parquet", index = False)
+print(daily_dataframe.head())
